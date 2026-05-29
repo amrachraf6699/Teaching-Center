634bf2b361a027c7474e937433ba10bdb7726a8e
Amr Achraf <amrachraf6690@icloud.com>
2026-05-30 00:21:24 +0300

    Fix factories

--- Modules/People/database/factories/StudentFactory.php
+++ Modules/People/database/factories/StudentFactory.php
@@ -3,38 +3,32 @@
 namespace Modules\People\Database\Factories;
 
 use App\Models\User;
-use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Modules\People\Models\Student;
 
+/**
+ * @extends Factory<Student>
+ */
 class StudentFactory extends Factory
 {
     protected $model = Student::class;
 
     public function definition(): array
     {
-        $faker = FakerFactory::create();
-
         return [
             'parent_id' => User::factory()->parent(),
 
-            'name' => $faker->name,
+            'name' => $this->faker->name,
 
-            'code' => strtoupper(sprintf(
-                'ST-%s%s-%d%d',
-                $faker->randomLetter,
-                $faker->randomLetter,
-                $faker->numberBetween(0, 9),
-                $faker->numberBetween(0, 9)
-            )),
+            'code' => Student::nextCode(),
 
-            'phone' => $faker->numerify('010########'),
+            'phone' => $this->faker->numerify('010########'),
 
-            'date_of_birth' => $faker
+            'date_of_birth' => $this->faker
                 ->dateTimeBetween('-18 years', '-8 years')
                 ->format('Y-m-d'),
 
-            'notes' => $faker->optional()->sentence,
+            'notes' => $this->faker->optional()->sentence,
 
             'is_active' => true,
         ];
@@ -42,7 +36,7 @@ class StudentFactory extends Factory
 
     public function forParent(User $parent): static
     {
-        return $this->state(fn (array $attributes): array => [
+        return $this->state(fn (): array => [
             'parent_id' => $parent->id,
         ]);
     }
