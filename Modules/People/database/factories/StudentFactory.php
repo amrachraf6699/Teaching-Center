da5f1de518d468c3e76548735eed929a4301873e
Amr Achraf <amrachraf6690@icloud.com>
2026-05-30 00:22:25 +0300

    Fix factories

--- Modules/People/database/factories/StudentFactory.php
+++ Modules/People/database/factories/StudentFactory.php
@@ -17,26 +17,24 @@ class StudentFactory extends Factory
     {
         return [
             'parent_id' => User::factory()->parent(),
-
-            'name' => $this->faker->name,
-
-            'code' => Student::nextCode(),
-
-            'phone' => $this->faker->numerify('010########'),
-
-            'date_of_birth' => $this->faker
-                ->dateTimeBetween('-18 years', '-8 years')
-                ->format('Y-m-d'),
-
-            'notes' => $this->faker->optional()->sentence,
-
+            'name' => fake()->name(),
+            'code' => strtoupper(sprintf(
+                'ST-%s%s-%d%d',
+                fake()->randomLetter(),
+                fake()->randomLetter(),
+                fake()->numberBetween(0, 9),
+                fake()->numberBetween(0, 9),
+            )),
+            'phone' => fake()->numerify('010########'),
+            'date_of_birth' => fake()->dateTimeBetween('-18 years', '-8 years')->format('Y-m-d'),
+            'notes' => fake()->optional()->sentence(),
             'is_active' => true,
         ];
     }
 
     public function forParent(User $parent): static
     {
-        return $this->state(fn (): array => [
+        return $this->state(fn (array $attributes): array => [
             'parent_id' => $parent->id,
         ]);
     }
