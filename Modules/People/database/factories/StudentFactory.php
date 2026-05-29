cc4737729fa718658bb1729c618b080951005f78
Amr Achraf <amrachraf6690@icloud.com>
2026-05-30 00:20:06 +0300

    Fix factories

--- Modules/People/database/factories/StudentFactory.php
+++ Modules/People/database/factories/StudentFactory.php
@@ -3,6 +3,7 @@
 namespace Modules\People\Database\Factories;
 
 use App\Models\User;
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Modules\People\Models\Student;
 
@@ -12,19 +13,29 @@ class StudentFactory extends Factory
 
     public function definition(): array
     {
+        $faker = FakerFactory::create();
+
         return [
             'parent_id' => User::factory()->parent(),
-            'name' => \fake()->name(),
+
+            'name' => $faker->name,
+
             'code' => strtoupper(sprintf(
                 'ST-%s%s-%d%d',
-                \fake()->randomLetter(),
-                \fake()->randomLetter(),
-                \fake()->numberBetween(0, 9),
-                \fake()->numberBetween(0, 9),
+                $faker->randomLetter,
+                $faker->randomLetter,
+                $faker->numberBetween(0, 9),
+                $faker->numberBetween(0, 9)
             )),
-            'phone' => \fake()->numerify('010########'),
-            'date_of_birth' => \fake()->dateTimeBetween('-18 years', '-8 years')->format('Y-m-d'),
-            'notes' => \fake()->optional()->sentence(),
+
+            'phone' => $faker->numerify('010########'),
+
+            'date_of_birth' => $faker
+                ->dateTimeBetween('-18 years', '-8 years')
+                ->format('Y-m-d'),
+
+            'notes' => $faker->optional()->sentence,
+
             'is_active' => true,
         ];
     }
