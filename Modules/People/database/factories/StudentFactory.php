182efe0aeb974747d74bbccc4fc9c627769e538e
Amr Achraf <amrachraf6690@icloud.com>
2026-05-30 00:17:44 +0300

    Fix factories

--- Modules/People/database/factories/StudentFactory.php
+++ Modules/People/database/factories/StudentFactory.php
@@ -17,17 +17,25 @@ class StudentFactory extends Factory
     {
         return [
             'parent_id' => User::factory()->parent(),
-            'name' => $this->faker->name(),
+
+            'name' => fake()->name(),
+
             'code' => strtoupper(sprintf(
                 'ST-%s%s-%d%d',
-                $this->faker->randomLetter(),
-                $this->faker->randomLetter(),
-                $this->faker->numberBetween(0, 9),
-                $this->faker->numberBetween(0, 9),
+                fake()->randomLetter(),
+                fake()->randomLetter(),
+                fake()->numberBetween(0, 9),
+                fake()->numberBetween(0, 9),
             )),
-            'phone' => $this->faker->numerify('010########'),
-            'date_of_birth' => $this->faker->dateTimeBetween('-18 years', '-8 years')->format('Y-m-d'),
-            'notes' => $this->faker->optional()->sentence(),
+
+            'phone' => fake()->numerify('010########'),
+
+            'date_of_birth' => fake()
+                ->dateTimeBetween('-18 years', '-8 years')
+                ->format('Y-m-d'),
+
+            'notes' => fake()->optional()->sentence(),
+
             'is_active' => true,
         ];
     }
